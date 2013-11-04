8a70ea38474ce3d4a441daa30cbaa67bfeda8da2
Winston <winston.yongwei@gmail.com>
2013-11-04 22:04:24 +0800

    Simple home page view.

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -1,9 +1,14 @@
 require "feature_spec_helper"
 
 feature "RubySG Home Page" do
+  let!(:events) { create_list(:event, 2) }
+
   scenario "user can see events" do
     visit root_path
 
-    page.should have_selector(".logo", text: "RubySG")
+    page.should have_selector("[data-test=event]", count: 2)
+    events.each do |event|
+      page.should have_selector("[data-test=event-title]", text: event.title)
+    end
   end
 end
