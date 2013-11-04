56b193d593e1402e6b76a031d3331154666881a9
Winston <winston.yongwei@gmail.com>
2013-11-04 22:41:52 +0800

    More layout changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_when(from, to)
+    if from.to_date == to.to_date
+      date        = from.strftime("%b %-e, %Y")
+      from_time   = from.strftime("%-l:%M%P")
+      to_time     = to.strftime("%-l:%M%P")
+      "#{date} - #{from_time} to #{to_time}"
+    else
+      from_string = from.strftime("%b %-e, %-l:%M%P")
+      to_string   = to.strftime("%b %-e, %-l:%M%P")
+      "#{from_string} to #{to_string}"
+    end
+  end
 end
