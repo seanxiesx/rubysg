291afcb37d1127b27a7145e201fed3ed3211bbfa
Winston <winston.yongwei@gmail.com>
2013-11-24 01:23:59 +0800

    Update styling for homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events =  Event.all
+    @upcoming_events =  Event.where("time >= ?", Time.now)
+    @historic_events =  Event.where("time < ?" , Time.now).limit(5)
   end
 end
---
+++ spec/features/home_spec.rb
@@ -0,0 +1,20 @@
+require "feature_spec_helper"
+
+feature "RubySG Home Page" do
+  let!(:upcoming_events) { create_list(:event, 2, time: 2.weeks.from_now) }
+  let!(:historic_events) { create_list(:event, 2, time: 2.weeks.ago) }
+
+  scenario "user can see events" do
+    visit root_path
+
+    page.should have_selector("[data-test=upcoming-event]", count: 2)
+    upcoming_events.each do |event|
+      page.should have_selector("[data-test=upcoming-event-name]", text: event.name)
+    end
+
+    page.should have_selector("[data-test=historic-event]", count: 2)
+    historic_events.each do |event|
+      page.should have_selector("[data-test=historic-event-name]", text: event.name)
+    end
+  end
+end
