bdc2e593cd2a1335dda5ccb56c1ae93de9c9832f
Simon Males <sime@sime.net.au>
2013-12-09 10:57:57 +0800

    Failing test for historic event links

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -16,7 +16,7 @@ feature "RubySG Home Page" do
     expect(page).to have_selector("[data-test=historic-event]", count: 2)
     historic_events.each do |event|
       expect(page).to have_selector("[data-test=historic-event-name]", text: event.name)
-      expect(page).to have_selector("[data-test=historic-event-link][href='#{event.event_url}']", text: "Details & Slides")
+      expect(page).to have_selector("[data-test=historic-event-link][href='#{event_path(event.id)}']", text: "Details & Slides")
     end
   end
 end
